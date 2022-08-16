f72859b18acd11130ee61c51b70e06c6876419ba
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2022-08-16 17:00:22 -0300

    feat(improves): ++ improves (#52)

--- website/docs/tutorial-extras/manage-docs-versions.md
+++
@@ -1,7 +0,0 @@
----
-sidebar_position: 1
----
-
-# Manage Docs Versions
-
-Docusaurus can manage multiple versions of your docs.
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,126 +1,118 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
-/** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'braintris 🧠',
+  title: "braintris 🧠",
   tagline: `Here that contain everything I've created and studied`,
-  url: 'https://biantris.dev', //wip
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'biantris', // Usually your GitHub org/user name.
-  projectName: 'braintris', // Usually your repo name.
+  url: "https://biantris.dev", //wip
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "biantris",
+  projectName: "braintris",
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      "@docusaurus/preset-classic",
+      {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          //editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sidebarPath: require.resolve("./sidebars.js"),
         },
-        articles: {
+        blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          //editUrl:'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
-      }),
+        googleAnalytics: {
+          trackingID: 'UA-141789564-1',
+        },
+      },
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Home',
-        // logo: {
-        //   alt: '',
-        //   src: '',
-        // },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Contents',
-          },
-          {to: '/articles', label: 'Articles', position: 'left'},
-          {to: '/articles', label: 'About', position: 'left'},
-          {
-            href: 'https://github.com/biantris/braintris',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Content',
-            items: [
-              {
-                label: 'Intro',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/biantris_',
-              },
-              {
-                label: 'LinkedIn',
-                href: 'https://www.linkedin.com/in/beatriiz-oliveiraa/',
-              },
-              // {
-              //   label: 'Discord',
-              //   href: 'https://discordapp.com/invite/docusaurus',
-              // },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Articles',
-                to: '/articles',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/biantris/braintris',
-              },
-              {
-                label: 'Dev.to',
-                href: 'https://dev.to/beatrizoliveira',
-              },
-              {
-                label: 'Medium',
-                href: 'https://beatrizoliveiraa.medium.com/',
-              }
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} biantris, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+  themeConfig: {
+    navbar: {
+      title: "Home",
+      // logo: {
+      //   alt: '',
+      //   src: '',
+      // },
+      items: [
+        {
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Contents",
+        },
+        { to: "/articles", label: "Articles", position: "left" },
+        { to: "/articles", label: "About", position: "left" },
+        {
+          href: "https://github.com/biantris/braintris",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Content",
+          items: [
+            {
+              label: "Intro",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Twitter",
+              href: "https://twitter.com/biantris_",
+            },
+            {
+              label: "LinkedIn",
+              href: "https://www.linkedin.com/in/beatriiz-oliveiraa/",
+            },
+            // {
+            //   label: 'Discord',
+            //   href: 'https://discordapp.com/invite/docusaurus',
+            // },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Articles",
+              to: "/articles",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/biantris/braintris",
+            },
+            {
+              label: "Dev.to",
+              href: "https://dev.to/beatrizoliveira",
+            },
+            {
+              label: "Medium",
+              href: "https://beatrizoliveiraa.medium.com/",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} biantris, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
 };
 
 module.exports = config;
